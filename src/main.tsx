e174dc0b5782e2a381be96a60d85c8634aa5c650
Ambadasugari <ambadasugariramvamshi@gmail.com>
2025-03-08 22:07:28 +0530

    Install Chakra Ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Provider } from "./components/ui/provider"; // Custom provider
+import React from "react";
+import ReactDOM from "react-dom/client"; // React 18 method for creating root
+import App from "./App"; // Main App component
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Create the root element and render the app
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider> {/* Custom Provider */}
+      <App /> {/* Main App component */}
+    </Provider>
+  </React.StrictMode>
+);
