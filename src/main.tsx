104fe1112856294888faf94e1e4aa9ddfe36db8a
Ambadasugari <ambadasugariramvamshi@gmail.com>
2025-03-09 16:30:00 +0530

    implement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { Provider } from "./components/ui/provider"; // Custom provider
 import React from "react";
 import ReactDOM from "react-dom/client"; // React 18 method for creating root
 import App from "./App"; // Main App component
-
+import theme from "./theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 // Create the root element and render the app
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider> {/* Custom Provider */}
-      <App /> {/* Main App component */}
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App></App>
+    </ChakraProvider>
   </React.StrictMode>
 );
